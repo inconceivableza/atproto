5a6be574a9721479bff6c38ad0acea74be020fee
David Fraser <davidf@inconceivable.co.za>
2025-06-13 16:54:29 +0200

    Use opentelemetry UndiciInstrumentation hook to add special child span to show we tried
    Add opentelemetry/api to help with dd-trace undici instrumentation
    Sometimes the traceparent doesn't come through, but then at least this shows that there was a request
    Other times, it works
    
    (cherry picked from commit faabe0f77fa338df9d241100f2547ce982c2e417)
    (cherry picked from commit 2d1c2a62386f58f7dfcaf1b148d95dfc6814c796)
    (cherry picked from commit 6021617e054bb98f72f724b755844aaaa2a8f1d7)

--- services/pds/tracer.js
+++ services/pds/tracer.js
@@ -4,30 +4,44 @@
 'use strict'
 
 const { registerInstrumentations } = require('@opentelemetry/instrumentation')
+const otel = require('@opentelemetry/api')
 const {
   BetterSqlite3Instrumentation,
 } = require('opentelemetry-plugin-better-sqlite3')
 const {
   UndiciInstrumentation
 } = require('@opentelemetry/instrumentation-undici')
-const { TracerProvider } = require('dd-trace') // Only works with commonjs
+const { TracerProvider, tracer } = require('dd-trace') // Only works with commonjs
   .init({ logInjection: true })
   .use('express', {
     hooks: { request: maintainXrpcResource },
   })
 
-const tracer = new TracerProvider()
-tracer.register()
+const tracerProvider = new TracerProvider()
+const otelTracer = otel.trace.getTracer('pds')
 
 registerInstrumentations({
-  tracerProvider: tracer,
+  tracerProvider: tracerProvider,
   instrumentations: [
     new BetterSqlite3Instrumentation(),
     new UndiciInstrumentation({
-      requestHook: (span, req) => { console.log("OPENTELEMETRY-UNDICI"); span.setTag('debug-propagate', 'pds-otel-trace'); },
+      requestHook: (span, req) => {
+        try {
+          const activeSpan = tracer.scope().active()
+          const childSpan = tracer.startSpan('undici.requestHook', {childOf: activeSpan})
+          childSpan.setTag('http.method', req.method)
+          childSpan.setTag('http.origin', req.origin)
+          childSpan.setTag('http.path', req.path)
+          childSpan.finish()
+        } catch (error) {
+          console.error("OpenTelemetry UndiciInstrumentation request hook error:", error);
+        }
+      },
     })
   ],
 })
+
+tracerProvider.register()
 
 const path = require('node:path')
 
