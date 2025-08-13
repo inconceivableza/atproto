cd47477bbbc3ec0628d57493b27650c3b162276b
NevilleVH <1nevillevh@gmail.com>
2025-08-13 15:49:21 +0200

    Fetch recipe records

--- packages/bsky/src/data-plane/server/routes/records.ts
+++ packages/bsky/src/data-plane/server/routes/records.ts
@@ -5,7 +5,7 @@ import { keyBy } from '@atproto/common'
 import { AtUri } from '@atproto/syntax'
 import { ids } from '../../../lexicon/lexicons'
 import { Service } from '../../../proto/bsky_connect'
-import { PostRecordMeta, Record } from '../../../proto/bsky_pb'
+import { GetRecipeRecordsRequest, PostRecordMeta, Record } from '../../../proto/bsky_pb'
 import { Database } from '../db'
 
 export default (db: Database): Partial<ServiceImpl<typeof Service>> => ({
@@ -17,6 +17,7 @@ export default (db: Database): Partial<ServiceImpl<typeof Service>> => ({
   getListItemRecords: getRecords(db, ids.AppBskyGraphListitem),
   getListRecords: getRecords(db, ids.AppBskyGraphList),
   getPostRecords: getPostRecords(db),
+  getRecipeRecords: getRecipeRecords(db),
   getProfileRecords: getRecords(db, ids.AppBskyActorProfile),
   getRepostRecords: getRecords(db, ids.AppBskyFeedRepost),
   getThreadGateRecords: getRecords(db, ids.AppBskyFeedThreadgate),
@@ -34,42 +35,49 @@ export default (db: Database): Partial<ServiceImpl<typeof Service>> => ({
 
 export const getRecords =
   (db: Database, collection?: string) =>
-  async (req: { uris: string[] }): Promise<{ records: Record[] }> => {
-    const validUris = collection
-      ? req.uris.filter((uri) => new AtUri(uri).collection === collection)
-      : req.uris
-    const res = validUris.length
-      ? await db.db
+    async (req: { uris: string[] }): Promise<{ records: Record[] }> => {
+      const validUris = collection
+        ? req.uris.filter((uri) => new AtUri(uri).collection === collection)
+        : req.uris
+      const res = validUris.length
+        ? await db.db
           .selectFrom('record')
           .selectAll()
           .where('uri', 'in', validUris)
           .execute()
-      : []
-    const byUri = keyBy(res, 'uri')
-    const records: Record[] = req.uris.map((uri) => {
-      const row = byUri.get(uri)
-      const json = row ? row.json : JSON.stringify(null)
-      const createdAtRaw = new Date(JSON.parse(json)?.['createdAt'])
-      const createdAt = !isNaN(createdAtRaw.getTime())
-        ? Timestamp.fromDate(createdAtRaw)
-        : undefined
-      const indexedAt = row?.indexedAt
-        ? Timestamp.fromDate(new Date(row?.indexedAt))
-        : undefined
-      const recordBytes = ui8.fromString(json, 'utf8')
-      return new Record({
-        record: recordBytes,
-        cid: row?.cid,
-        createdAt,
-        indexedAt,
-        sortedAt: compositeTime(createdAt, indexedAt),
-        takenDown: !!row?.takedownRef,
-        takedownRef: row?.takedownRef ?? undefined,
-        tags: row?.tags ?? undefined,
+        : []
+      const byUri = keyBy(res, 'uri')
+      const records: Record[] = req.uris.map((uri) => {
+        const row = byUri.get(uri)
+        const json = row ? row.json : JSON.stringify(null)
+        const createdAtRaw = new Date(JSON.parse(json)?.['createdAt'])
+        const createdAt = !isNaN(createdAtRaw.getTime())
+          ? Timestamp.fromDate(createdAtRaw)
+          : undefined
+        const indexedAt = row?.indexedAt
+          ? Timestamp.fromDate(new Date(row?.indexedAt))
+          : undefined
+        const recordBytes = ui8.fromString(json, 'utf8')
+        return new Record({
+          record: recordBytes,
+          cid: row?.cid,
+          createdAt,
+          indexedAt,
+          sortedAt: compositeTime(createdAt, indexedAt),
+          takenDown: !!row?.takedownRef,
+          takedownRef: row?.takedownRef ?? undefined,
+          tags: row?.tags ?? undefined,
+        })
       })
-    })
-    return { records }
+      return { records }
+    }
+
+export const getRecipeRecords = (db: Database) => {
+  const getBaseRecords = getRecords(db, ids.AppFoodiosFeedRecipePost)
+  return function (req: GetRecipeRecordsRequest) {
+    return getBaseRecords(req)
   }
+}
 
 export const getPostRecords = (db: Database) => {
   const getBaseRecords = getRecords(db, ids.AppBskyFeedPost)
@@ -80,16 +88,16 @@ export const getPostRecords = (db: Database) => {
       getBaseRecords(req),
       req.uris.length
         ? await db.db
-            .selectFrom('post')
-            .where('uri', 'in', req.uris)
-            .select([
-              'uri',
-              'violatesThreadGate',
-              'violatesEmbeddingRules',
-              'hasThreadGate',
-              'hasPostGate',
-            ])
-            .execute()
+          .selectFrom('post')
+          .where('uri', 'in', req.uris)
+          .select([
+            'uri',
+            'violatesThreadGate',
+            'violatesEmbeddingRules',
+            'hasThreadGate',
+            'hasPostGate',
+          ])
+          .execute()
         : [],
     ])
     const byKey = keyBy(details, 'uri')
