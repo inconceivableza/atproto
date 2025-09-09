c4e08fd9a241c0ebf28901d4a5f2e6ccb32d1d37
NevilleVH <1nevillevh@gmail.com>
2025-09-09 16:33:18 +0200

    Update getRecipeRecords to include revisions

--- packages/bsky/src/data-plane/server/routes/records.ts
+++ packages/bsky/src/data-plane/server/routes/records.ts
@@ -5,8 +5,10 @@ import { keyBy } from '@atproto/common'
 import { AtUri } from '@atproto/syntax'
 import { ids } from '../../../lexicon/lexicons'
 import { Service } from '../../../proto/bsky_connect'
-import { GetRecipeRecordsRequest, PostRecordMeta, Record } from '../../../proto/bsky_pb'
+import { GetRecipeRecordsRequest, PostRecordMeta, Record as ATRecord, RecipeRecord, RecipeRevisionRecord } from '../../../proto/bsky_pb'
 import { Database } from '../db'
+import { Record as BskyRecord } from '../db/tables/record'
+import { Selectable, } from 'kysely'
 
 export default (db: Database): Partial<ServiceImpl<typeof Service>> => ({
   getBlockRecords: getRecords(db, ids.AppBskyGraphBlock),
@@ -35,7 +37,7 @@ export default (db: Database): Partial<ServiceImpl<typeof Service>> => ({
 
 export const getRecords =
   (db: Database, collection?: string) =>
-    async (req: { uris: string[] }): Promise<{ records: Record[] }> => {
+    async (req: { uris: string[] }): Promise<{ records: ATRecord[] }> => {
       const validUris = collection
         ? req.uris.filter((uri) => new AtUri(uri).collection === collection)
         : req.uris
@@ -47,7 +49,7 @@ export const getRecords =
           .execute()
         : []
       const byUri = keyBy(res, 'uri')
-      const records: Record[] = req.uris.map((uri) => {
+      const records: ATRecord[] = req.uris.map((uri) => {
         const row = byUri.get(uri)
         const json = row ? row.json : JSON.stringify(null)
         const createdAtRaw = new Date(JSON.parse(json)?.['createdAt'])
@@ -58,7 +60,7 @@ export const getRecords =
           ? Timestamp.fromDate(new Date(row?.indexedAt))
           : undefined
         const recordBytes = ui8.fromString(json, 'utf8')
-        return new Record({
+        return new ATRecord({
           record: recordBytes,
           cid: row?.cid,
           createdAt,
@@ -72,10 +74,77 @@ export const getRecords =
       return { records }
     }
 
+function makeRecordInfo(row: Selectable<BskyRecord>) {
+  const createdAtRaw = new Date(JSON.parse(row.json)?.['createdAt'])
+  const createdAt = !isNaN(createdAtRaw.getTime())
+    ? Timestamp.fromDate(createdAtRaw)
+    : undefined
+  const indexedAt = row?.indexedAt
+    ? Timestamp.fromDate(new Date(row?.indexedAt))
+    : undefined
+  return {
+    uri: row.uri,
+    cid: row?.cid,
+    createdAt,
+    indexedAt,
+    sortedAt: compositeTime(createdAt, indexedAt),
+    takenDown: !!row?.takedownRef,
+    takedownRef: row?.takedownRef ?? undefined,
+    tags: row?.tags ?? undefined,
+  }
+}
+
 export const getRecipeRecords = (db: Database) => {
   const getBaseRecords = getRecords(db, ids.AppFoodiosFeedRecipePost)
-  return function (req: GetRecipeRecordsRequest) {
-    return getBaseRecords(req)
+  return async function (req: GetRecipeRecordsRequest) {
+    // TODO: this could be optimized with a join + group by
+
+    // await db.db
+    //       .selectFrom('record')
+    //       .select([sql<string>`ARRAY_AGG`.as('')])
+    //       .where('uri', 'in', req.uris)
+    //       .innerJoin("recipe_revision", "record.uri", "recipe_revision.recipePostUri")
+    //       .execute()
+
+    const revisionUris = await db.db.selectFrom("recipe_revision")
+      .select("uri")
+      .where("recipePostUri", "in", req.uris)
+      .execute()
+    const recipesAndRevisions = await db.db
+      .selectFrom('record')
+      .selectAll()
+      .where('uri', 'in', req.uris.concat(revisionUris.map(r => r.uri)))
+      .execute()
+
+    const recsByUri: Record<string, RecipeRecord> = {}
+    const revisionsByUri: Record<string, RecipeRevisionRecord[]> = {}
+    recipesAndRevisions.forEach(row => {
+      const atUri = new AtUri(row.uri)
+      if (atUri.collection === ids.AppFoodiosFeedRecipePost) {
+        const json = row.json
+        const recordBytes = ui8.fromString(json, 'utf8')
+        recsByUri[row.uri] = new RecipeRecord({
+          recordInfo: makeRecordInfo(row),
+          record: recordBytes,
+        })
+
+      } else if (atUri.collection === ids.AppFoodiosFeedRecipePost) {
+        const recordBytes = ui8.fromString(row.json, 'utf8')
+        const revs = revisionsByUri[row.uri] ??= []
+        revs.push(new RecipeRevisionRecord({
+          recordInfo: makeRecordInfo(row),
+          record: recordBytes
+        }))
+      }
+    })
+    const records = req.uris.map(uri => {
+      const recipeRecord = recsByUri[uri] ?? new RecipeRecord()
+      const revisions = revisionsByUri[uri] ?? []
+      recipeRecord.revisions = revisions
+      return recipeRecord
+    })
+
+    return { records }
   }
 }
 
@@ -83,7 +152,7 @@ export const getPostRecords = (db: Database) => {
   const getBaseRecords = getRecords(db, ids.AppBskyFeedPost)
   return async (req: {
     uris: string[]
-  }): Promise<{ records: Record[]; meta: PostRecordMeta[] }> => {
+  }): Promise<{ records: ATRecord[]; meta: PostRecordMeta[] }> => {
     const [{ records }, details] = await Promise.all([
       getBaseRecords(req),
       req.uris.length
