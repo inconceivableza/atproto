5a0afd33641211a0630db949925ae4c669ac6562
NevilleVH <1nevillevh@gmail.com>
2025-09-19 16:27:29 +0200

    Change at uri validation to allow search paramaters

--- packages/syntax/src/aturi_validation.ts
+++ packages/syntax/src/aturi_validation.ts
@@ -1,6 +1,7 @@
-import { ensureValidDid, ensureValidDidRegex } from './did'
-import { ensureValidHandle, ensureValidHandleRegex } from './handle'
-import { ensureValidNsid, ensureValidNsidRegex } from './nsid'
+import { AtUri } from './aturi'
+import { ensureValidDidRegex } from './did'
+import { ensureValidHandleRegex } from './handle'
+import { ensureValidNsidRegex } from './nsid'
 
 // Human-readable constraints on ATURI:
 //   - following regular URLs, a 8KByte hard total length limit
@@ -16,83 +17,8 @@ import { ensureValidNsid, ensureValidNsidRegex } from './nsid'
 //      - rkey must have at least one char
 //      - regardless of path component, a fragment can follow  as "#" and then a JSON pointer (RFC-6901)
 export const ensureValidAtUri = (uri: string) => {
-  // JSON pointer is pretty different from rest of URI, so split that out first
-  const uriParts = uri.split('#')
-  if (uriParts.length > 2) {
-    throw new Error('ATURI can have at most one "#", separating fragment out')
-  }
-  const fragmentPart = uriParts[1] || null
-  uri = uriParts[0]
-
-  // check that all chars are boring ASCII
-  if (!/^[a-zA-Z0-9._~:@!$&')(*+,;=%/-]*$/.test(uri)) {
-    throw new Error('Disallowed characters in ATURI (ASCII)')
-  }
-
-  const parts = uri.split('/')
-  if (parts.length >= 3 && (parts[0] !== 'at:' || parts[1].length !== 0)) {
-    throw new Error('ATURI must start with "at://"')
-  }
-  if (parts.length < 3) {
-    throw new Error('ATURI requires at least method and authority sections')
-  }
-
-  try {
-    if (parts[2].startsWith('did:')) {
-      ensureValidDid(parts[2])
-    } else {
-      ensureValidHandle(parts[2])
-    }
-  } catch {
-    throw new Error('ATURI authority must be a valid handle or DID')
-  }
-
-  if (parts.length >= 4) {
-    if (parts[3].length === 0) {
-      throw new Error(
-        'ATURI can not have a slash after authority without a path segment',
-      )
-    }
-    try {
-      ensureValidNsid(parts[3])
-    } catch {
-      throw new Error(
-        'ATURI requires first path segment (if supplied) to be valid NSID',
-      )
-    }
-  }
-
-  if (parts.length >= 5) {
-    if (parts[4].length === 0) {
-      throw new Error(
-        'ATURI can not have a slash after collection, unless record key is provided',
-      )
-    }
-    // would validate rkey here, but there are basically no constraints!
-  }
-
-  if (parts.length >= 6) {
-    throw new Error(
-      'ATURI path can have at most two parts, and no trailing slash',
-    )
-  }
-
-  if (uriParts.length >= 2 && fragmentPart == null) {
-    throw new Error('ATURI fragment must be non-empty and start with slash')
-  }
-
-  if (fragmentPart != null) {
-    if (fragmentPart.length === 0 || fragmentPart[0] !== '/') {
-      throw new Error('ATURI fragment must be non-empty and start with slash')
-    }
-    // NOTE: enforcing *some* checks here for sanity. Eg, at least no whitespace
-    if (!/^\/[a-zA-Z0-9._~:@!$&')(*+,;=%[\]/-]*$/.test(fragmentPart)) {
-      throw new Error('Disallowed characters in ATURI fragment (ASCII)')
-    }
-  }
-
-  if (uri.length > 8 * 1024) {
-    throw new Error('ATURI is far too long')
+  if (!new AtUri(uri)) {
+    throw new Error('Invalid at-uri')
   }
 }
 
