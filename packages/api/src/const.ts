6b1ffdde7c88eb33649ad61afb74acb3bc2a893c
David Fraser <davidf@inconceivable.co.za>
2026-01-22 15:48:09 +0200

    Allow the default Bluesky BSKY_LABELER_DID to be overridden by an environment variable This should be set if a different base moderator is to be supplied, for any of the services that use the api agent

--- packages/api/src/const.ts
+++ packages/api/src/const.ts
@@ -1,1 +1,3 @@
-export const BSKY_LABELER_DID = 'did:plc:ar7c4by46qjdydhdevvrndac'
+// This is used to include this as the default labeler for the atproto api agent
+// and also from within social-app
+export const BSKY_LABELER_DID = process.env.BSKY_LABELER_DID ?? 'did:plc:ar7c4by46qjdydhdevvrndac'
